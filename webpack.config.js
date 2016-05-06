4131242f4b917da591fcaeb2994c63784d209858
jose bustamante <jose.bustamante@H2015-006s-iMac.local>
2016-05-06 18:16:39 +0200

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+modele.export = {
+  entry: './main.js',
+  output: {
+    path: './',
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 3000
+  },
+  module: {
+    loader: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+}
